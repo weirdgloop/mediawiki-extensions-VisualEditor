efc42c3f3c91dfe9fb1d88bd0464bbfbd83faed0
Bartosz Dziewoński <matma.rex@gmail.com>
2019-05-13 20:37:11 +0200

    Allow switching from WTE to VE with changes in non-RESTBase mode
    * Change the query in ve.init.mw.ArticleTargetLoader#requestParsoidData
      so that in non-RESTBase mode with wikitext it still returns the
      metadata required to initialize the editor, using the backend API
      code added in I1b35b28e428a1f86d2e34d90ddbe73361ce14818. This fixes
      the exception from T222312.
    
    * Introduce new configuration option $wgVisualEditorAllowLossySwitching
      to control this feature. It is enabled by default, fixing T214542.
      We allow it to be disabled because switching in non-RESTBase mode may
      cause "dirty diffs" (non-semantic changes to the wikitext), which are
      undesirable on wikis where users carefully review all changes.
    
    Bug: T214542
    Bug: T222312
    Change-Id: I58879cba5612002c70c24731306214d2577c2c52

--- modules/ve-mw/init/ve.init.mw.ArticleTarget.js
+++ modules/ve-mw/init/ve.init.mw.ArticleTarget.js
@@ -2455,6 +2455,8 @@ ve.init.mw.ArticleTarget.prototype.switchToFallbackWikitextEditor = function () 
  */
 ve.init.mw.ArticleTarget.prototype.switchToVisualEditor = function () {
 	var dataPromise, windowManager, switchWindow,
+		config = mw.config.get( 'wgVisualEditorConfig' ),
+		canSwitch = config.fullRestbaseUrl || config.allowLossySwitching,
 		target = this;
 
 	if ( !this.edited ) {
@@ -2464,9 +2466,7 @@ ve.init.mw.ArticleTarget.prototype.switchToVisualEditor = function () {
 
 	// Show a discard-only confirm dialog, and then reload the whole page, if
 	// the server can't switch for us because that's not supported.
-	if (
-		!mw.config.get( 'wgVisualEditorConfig' ).fullRestbaseUrl
-	) {
+	if ( !canSwitch ) {
 		windowManager = new OO.ui.WindowManager();
 		switchWindow = new mw.libs.ve.SwitchConfirmDialog();
 		$( 'body' ).append( windowManager.$element );
